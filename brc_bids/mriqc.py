dd16b7bd8d6bcc2a6ef3339bf9947861ba1b1b9e
Martin Craig <martin.craig@nottingham.ac.uk>
2022-09-13 15:57:01 +0100

    Add mriqc which runs on the entire unmodified bids dataset

---
+++ brc_bids/mriqc.py
@@ -0,0 +1,15 @@
+"""
+BRC_BIDS: Code to run structural pipeline
+"""
+import logging
+import subprocess
+
+LOG = logging.getLogger(__name__)
+
+from . import utils
+
+SINGULARITY_IMAGE="/software/imaging/singularity_images/poldracklab_mriqc-2021-01-30-767af1135fae.simg"
+
+def run(bidsdir, outdir, cluster=False, dep_job=None):
+    cmd = ['singularity', 'run', '--cleanenv', SINGULARITY_IMAGE, bidsdir, outdir, 'participant', '--no-sub']
+    return utils.submit_cmd(cmd, cluster, dep_job)
